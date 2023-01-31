d7d33f9ed83b7a236ddcc428a5c412db2ffa1d26
domagoj.vukovic <off.vukovic@gmail.com>
2023-01-31 16:50:02 +0100

    refactor(eslint): allow `warn` and `error` for `no-console` rule

--- eslint/core.js
+++ eslint/core.js
@@ -284,7 +284,12 @@ module.exports = {
         'no-compare-neg-zero': 'error',
         'no-cond-assign': 'error',
         'no-confusing-arrow': 'error',
-        'no-console': 'error',
+        'no-console': [
+            'error',
+            {
+                allow: ['warn', 'error'],
+            },
+        ],
         'no-const-assign': 'error',
         'no-constant-condition': 'error',
         'no-control-regex': 'error',
